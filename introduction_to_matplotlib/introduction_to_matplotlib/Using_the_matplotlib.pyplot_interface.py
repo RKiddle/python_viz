3857a25c5ff5e1cec2735f36344ae6c0c979f69a
RKiddle <RKiddle@users.noreply.github.com>
2019-07-23 20:18:06 +0700

    Create Using_the_matplotlib.pyplot_interface.py

---
+++ introduction_to_matplotlib/introduction_to_matplotlib/Using_the_matplotlib.pyplot_interface.py
@@ -0,0 +1,8 @@
+# Import the matplotlib.pyplot submodule and name it plt
+import ____ as ____
+
+# Create a Figure and an Axes with plt.subplots
+fig, ax = ____
+
+# Call the show function to show the result
+____
